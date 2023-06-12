96e78dee04853309e276bf30c7a6399759de0b1c
heckfy86 <marlinco263@gmail.com>
2023-06-12 12:51:56 +0200

    Product Laptop

--- src/main/java/com/gabdra/ru/IProduct.java
+++
@@ -1,7 +0,0 @@
-package com.gabdra.ru;
-
-public interface IProduct {
-    void installFirstPart(IProductPart firstProductPart);
-    void installSecondPart(IProductPart secondProductPart);
-    void installThirdPart(IProductPart thirdProductPart);
-}
--- src/main/java/com/gabdra/ru/IProductPart.java
+++
@@ -1,4 +0,0 @@
-package com.gabdra.ru;
-
-public class IProductPart {
-}
